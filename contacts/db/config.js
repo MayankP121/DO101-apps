e12e098ce2afdadfb96f6027e9819473bed11147
MayankP121 <mayankpuniwala2005@gmail.com>
2025-01-24 22:48:11 +0530

    # 6

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@contactsdb:5432/contactsdb';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
     ssl: false,
 });
-  
+
 module.exports = { pgconn }
